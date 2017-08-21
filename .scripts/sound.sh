3377189ded6133d7205310e6b75a7b3e37de92b7
Robert Sandor <sandorrobertk94@gmail.com>
2017-08-21 06:09:07 +0300

    minor changes to sound script

--- .scripts/sound.sh
+++ .scripts/sound.sh
@@ -4,12 +4,18 @@ action=$1
 
 offset=${2:-5}
 
+icon=""
 if [ "$action" == "up" ]; then
 	amixer -q set Master $offset%+ unmute
 elif [ "$action" == "down" ]; then
 	amixer -q set Master $offset%- unmute
+	icon=""
 elif [ "$action" == "mute" ]; then
 	amixer -q set Master toggle
+	status=$(amixer -M sget Master | grep "Front Left:" | awk '{print $6}' | sed 's/[^a-z]//g')
+	if [ "$status" == "off" ]; then
+		icon=""
+	fi
 else
 	echo "Usage $0 [ up | down | mute ] {offset}"
 	exit 1
@@ -20,9 +26,9 @@ volume=$(amixer -M sget Master | grep "Front Left:" | awk '{print $5}' | sed 's/
 DUNST_VOLUME_FILE=/tmp/.dunst_volume
 dunst_id=$(cat $DUNST_VOLUME_FILE)
 if [ $dunst_id -gt "0" ]; then
-	dunstify -p -r $dunst_id "Volume: $volume" > $DUNST_VOLUME_FILE
+	dunstify -p -r $dunst_id "$icon $volume" -t 200 > $DUNST_VOLUME_FILE
 else
-	dunstify -p "Volume: $volume" > $DUNST_VOLUME_FILE
+	dunstify -p "$icon $volume" -t 200 > $DUNST_VOLUME_FILE
 fi
 
 
