40958da8965abc063540ee6b78b308da2713bdc3
Robert Sandor <sandorrobertk94@gmail.com>
2017-09-01 12:29:08 +0300

    polybar script on every connected display, and other small changes

--- .config/polybar/startup.sh
+++ .config/polybar/startup.sh
@@ -6,8 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar main_bar &
-MONITOR=HDMI1 polybar main_bar &
+# Get all of the connected monitors
+connected=`xrandr | grep " connected" | awk '{print $1}'`
+
+# Launch a bar on every display
+for display in $connected
+do
+    MONITOR=$display polybar main_bar &
+done
 
 echo "Bars launched..."
