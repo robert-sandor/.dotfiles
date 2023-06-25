80ae43fe1b68405255fa1e9237c3aaee0e079eed
Robert Sandor <sandorrobertk94@gmail.com>
2023-06-25 14:39:56 +0300

    moved tmux-sessionizer, add prompts to fzf-tmux

--- dot_config/tmux/scripts/executable_tmux-sessionizer.sh
+++ dot_config/tmux/scripts/executable_tmux-sessionizer.sh
@@ -1,22 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ $# -eq 1 ]]; then
-	selected=$1
-else
-	selected=$(fd -d 1 -t d . ~ ~/dev ~/.config | fzf-tmux -p)
-fi
+selected=$(fd -d 1 -t d . ~ ~/dev ~/.config | fzf-tmux -p --prompt "Select directory > ")
 
 if [[ -z $selected ]]; then
 	exit 0
 fi
 
 selected_name=$(basename "$selected" | tr . _)
-tmux_running=$(pgrep tmux)
-
-if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-	tmux new-session -s "$selected_name" -c "$selected"
-	exit 0
-fi
 
 if ! tmux has-session -t="$selected_name" 2>/dev/null; then
 	tmux new-session -ds "$selected_name" -c "$selected"
