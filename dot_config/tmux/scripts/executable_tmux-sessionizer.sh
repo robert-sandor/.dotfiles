bf2d07051342b205c8f3d4f858e3f5f0abe47847
Robert Sandor <sandorrobertk94@gmail.com>
2023-06-25 14:55:10 +0300

    label fzf prompts, use sessionizer as tm alias

--- dot_config/tmux/scripts/executable_tmux-sessionizer.sh
+++ dot_config/tmux/scripts/executable_tmux-sessionizer.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
-selected=$(fd -d 1 -t d . ~ ~/dev ~/.config | fzf-tmux -p --prompt "Select directory > ")
+selected=$(fd -d 1 -t d . ~ ~/dev ~/.config | fzf-tmux -p --border-label " Switch to directory ")
 
 if [[ -z $selected ]]; then
 	exit 0
 fi
 
 selected_name=$(basename "$selected" | tr . _)
+tmux_running=$(pgrep tmux)
+
+if [[ -z $tmux_running ]]; then
+	tmux new-session -s "$selected_name" -c "$selected"
+	exit 0
+fi
 
 if ! tmux has-session -t="$selected_name" 2>/dev/null; then
 	tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
-tmux switch-client -t "$selected_name"
+if [[ -z $TMUX ]]; then
+	tmux attach -t "$selected_name"
+else
+	tmux switch-client -t "$selected_name"
+fi
