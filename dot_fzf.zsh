23f6a8b77afc4e9faf0dea7c17d7895fa8ed007f
Robert Sandor <sandorrobertk94@gmail.com>
2023-07-30 14:56:33 +0300

    change to zgenom from oh-my-zsh

---
+++ dot_fzf.zsh
@@ -0,0 +1,19 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == *${FZF_PATH}/bin* ]]; then
+  export PATH="$PATH:${FZF_PATH}/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "${FZF_PATH}/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "${FZF_PATH}/shell/key-bindings.zsh"
+
+# theme: catppuccin-mocha
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
