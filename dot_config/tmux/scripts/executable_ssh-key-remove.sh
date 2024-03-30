6a551e6d2f0eb576ae7fd36889707fa19cceba72
Robert Sandor <sandorrobertk94@gmail.com>
2024-03-30 23:59:06 +0200

    update scripts to be sh compatible

--- dot_config/tmux/scripts/executable_ssh-key-remove.sh
+++ dot_config/tmux/scripts/executable_ssh-key-remove.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 if [[ $# -eq 1 ]]; then
-	ssh-keygen -R "$1"
-	exit $?
+  ssh-keygen -R "$1"
+  exit $?
 fi
 
-selected=$(cat ~/.ssh/known_hosts | cut -d ' ' -f1 | uniq | fzf-tmux -p --border-label " Choose host to remove ")
+selected=$(cut -d ' ' -f1 <~/.ssh/known_hosts | uniq | fzf-tmux -p --border-label " Choose host to remove ")
 
 if [[ -z $selected ]]; then
-	exit 0
+  exit 0
 fi
 
 ssh-keygen -R "$selected"
--- dot_config/tmux/scripts/executable_tmux-sessionizer.sh
+++ dot_config/tmux/scripts/executable_tmux-sessionizer.sh
@@ -1,26 +1,26 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # selected=$(fd -d 1 -t d . ~ ~/dev ~/.config | fzf-tmux -p --border-label " Switch to directory ")
 selected=$(zoxide query -l | fzf-tmux -p --border-label " Switch to directory ")
 
-if [[ -z $selected ]]; then
-	exit 0
+if [ -z "$selected" ]; then
+  exit 0
 fi
 
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
-if [[ -z $tmux_running ]]; then
-	tmux new-session -s "$selected_name" -c "$selected"
-	exit 0
+if [ -z "$tmux_running" ]; then
+  tmux new-session -s "$selected_name" -c "$selected"
+  exit 0
 fi
 
 if ! tmux has-session -t="$selected_name" 2>/dev/null; then
-	tmux new-session -ds "$selected_name" -c "$selected"
+  tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
-if [[ -z $TMUX ]]; then
-	tmux attach -t "$selected_name"
+if [ -z "$TMUX" ]; then
+  tmux attach -t "$selected_name"
 else
-	tmux switch-client -t "$selected_name"
+  tmux switch-client -t "$selected_name"
 fi
