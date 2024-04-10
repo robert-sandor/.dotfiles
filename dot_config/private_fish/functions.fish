dfab992605b9d7f92b55ca2e2b73282819b52deb
Robert Sandor <sandorrobertk94@gmail.com>
2024-04-10 23:52:22 +0300

    add fish function to manage dotfiles

--- dot_config/private_fish/functions.fish
+++ dot_config/private_fish/functions.fish
@@ -41,3 +41,12 @@ function lg -d 'lazygit with tmux' -w lazygit
         lazygit $argv
     end
 end
+
+function dotfiles -d 'manage dotfiles'
+    if test -f "$argv[1]"
+        chezmoi edit -a --watch "$argv[1]"
+    else
+        set file (chezmoi managed -i files -p absolute | fzf -q "$argv[1]")
+        test -n "$file"; and chezmoi edit -a --watch "$file"
+    end
+end
